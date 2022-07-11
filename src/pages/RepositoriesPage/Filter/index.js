715cb44bde2b5413f3aae7685898738b02a32ac0
Saulo Coelho <coelho.saulo@gmail.com>
2022-07-10 21:50:36 -0300

    adicionado component filter pt1

--- src/pages/RepositoriesPage/Filter/index.js
+++ src/pages/RepositoriesPage/Filter/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Selector, Cleaner } from './styles';
 
-const Filter = () => {
-  const langs = [
-    { name: 'HTML', count: 5, color: '#f1c40f' },
-    { name: 'ReactNative', count: 5, color: '#95a5a6' },
-    { name: 'Python', count: 10, color: '#3498db' },
-    { name: 'React', count: 8, color: '#2ecc71' },
-  ];
-
-  const selectors = langs.map(({ name, count, color }) => (
+const Filter = ({ languages }) => {
+  const selectors = languages.map(({ name, count, color }) => (
     <Selector key={name.toLocaleLowerCase()} color={color}>
       <span>{name}</span>
       <span>{count}</span>
@@ -24,4 +18,15 @@ const Filter = () => {
     </Container>
   );
 };
+
+Filter.propTypes = {
+  languages: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+      color: PropTypes.string,
+    }).isRequired
+  ).isRequired,
+};
+
 export default Filter;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -18,11 +18,41 @@ const RepositoriesPage = () => {
     location: 'Bauru - SP',
   };
 
+  // eslint-disable-next-line no-unused-vars
+  const repositories = [
+    {
+      name: 'Repo1',
+      description: 'Descrição',
+      html_url: 'https://google.com.br',
+      language: 'Python',
+    },
+    {
+      name: 'Repo2',
+      description: 'Descrição',
+      html_url: 'https://google.com.br',
+      language: 'React',
+    },
+    {
+      name: 'Repo3',
+      description: 'Descrição',
+      html_url: 'https://google.com.br',
+      language: 'ReactNative',
+    },
+  ];
+
+  // calculo dos filters
+  const languages = [
+    { name: 'HTML', count: 5, color: '#f1c40f' },
+    { name: 'ReactNative', count: 5, color: '#95a5a6' },
+    { name: 'Python', count: 10, color: '#3498db' },
+    { name: 'React', count: 8, color: '#2ecc71' },
+  ];
+
   return (
     <Container>
       <Sidebar>
         <Profile user={user} />
-        <Filter />
+        <Filter languages={languages} />
       </Sidebar>
       <Main>
         <Repositories />
